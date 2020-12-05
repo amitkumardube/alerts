e4d1ccaf277af96a44926612d24e3745e8b10aaa
amitkumardube <amitkumardube@gmail.com>
2020-12-05 21:29:15 +0000

    added file structure for alter setup

--- config/read_config.go
+++ config/read_config.go
@@ -1,22 +1,29 @@
 package config
 
-type config struct {
-	database_config database_config `json:"database_config,omitempty"`
+type Config struct {
+	database_config Database_config `json:"database_config,omitempty"`
+	alert_config []Alert_config `json:"alert_config,omitempty"`
+	email_config Email_config `json:"email_config,omitempty"`
 }
 
-type database_config struct {
-	:w
-
+type Database_config struct {
+	database_type string `json:"database_type,omitempty"`
+	host string `json:"host,omitempty"`
+	user string `json:"user,omitempty"`
+	password string `json:"password,omitempty"`
+	database string `json:"database,omitempty"`
 }
 
-type alert_config struct {
-	table
+type Alert_config struct {
+	table_name string `json:"table_name,omitempty"`
+	column_name string `json:"column_name,omitempty"`
+	expiry_alert_threshold_days int `json:"expiry_alert_threshold_days,omitempty"`
+	stock_alert_threshold_quantity int `json:"stock_alert_threshold_quantity,omitempty"`
 }
 
-type email_config struct {
-
-
-} 
+type Email_config struct {
+	smtp string `json:"smpt,omitempty"`
+}
 
 
 
